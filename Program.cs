315202272578650456d1918f370dcf7a01edbae9
PaildarBasanthi <basanthisrec@gmail.com>
2023-07-22 11:07:43 +0530

    first Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "John!";
+            Console.WriteLine($"Hello {a}");
+        }
+    }
+}
